912dc4e3369cf94b20bfef5e0b7d525fa82c399b
valisaros <valeriu.saros@gmail.com>
2020-04-02 11:46:55 +0200

    Create Maximum Pairwise Product.py

---
+++ Data Structures and Algorithms Specialization/Algorithmic Toolbox/Maximum Pairwise Product/Maximum Pairwise Product.py
@@ -0,0 +1,22 @@
+# Make the function
+
+def max_pairwise_product_fast(n, numbers):
+    max_index1 = -1
+    for i in range(n):
+        if max_index1 == -1 or numbers[i] > numbers[max_index1]:
+            max_index1 = i
+
+    max_index2 = -1
+    for i in range(n):
+        if i != max_index1 and (max_index2 == -1 or numbers[i] > numbers[max_index2]):
+            max_index2 = i
+
+    return numbers[max_index1] * numbers[max_index2]
+
+
+if __name__ == '__main__':
+    n = int(input())
+    a = [int(x) for x in input().split()]
+    assert (len(a) == n)
+
+    print(max_pairwise_product_fast(n, a))
