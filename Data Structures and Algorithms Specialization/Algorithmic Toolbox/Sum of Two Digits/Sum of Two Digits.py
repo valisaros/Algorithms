3c7f542c49307d0699a51d173237adb0e5d775ce
valisaros <valeriu.saros@gmail.com>
2020-04-06 14:13:29 +0200

    Delete Sum of Two Digits.py

--- Data Structures and Algorithms Specialization/Algorithmic Toolbox/Sum of Two Digits/Sum of Two Digits.py
+++
@@ -1,8 +0,0 @@
-# We make a function for sum
-
-def sum(first_digit, second_digit):
-    return (first_digit + second_digit)
-
-# Taking two inputs at a time
-A, B = map(int, input().split())
-print(sum(A, B))
