9512e32122c01958e179e3099df747cd12af0013
valisaros <valeriu.saros@gmail.com>
2020-06-15 10:21:40 +0400

    Create Count Primes.py

---
+++ Tests/2. Methods and Functions/4. Challenge/Count Primes.py
@@ -0,0 +1,31 @@
+# Write a function that returns the number of prime numbers that exist up to and including a given number
+# By convention we'll treat 0 and 1 as not prime.
+
+def count_primes(num):
+
+    # Check for 0 or 1 input
+
+    if num < 2:
+        return 0
+
+
+    # 2 or greater
+
+    # Store our prime numbers
+    primes = [2]
+    # Counter going up to the input number
+    x = 3
+
+    # x is going through every number up to input number
+    while x <= num:
+        # Check if x is prime
+        for y in range(3,x,2):
+            if x % y == 0:
+                x += 2
+                break
+        else:
+            primes.append(x)
+            x += 2
+
+    print(primes)
+    return len(primes)
